7f0e397704035d73e1f20788cfd15599443a1242
misamisa-create <missmis0329@gmail.com>
2021-08-01 04:40:07 +0000

    [Update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   attachment :profile_image
 
+  has_many :reverse_of_relationships,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy
+  has_many :followers,through: :reverse_of_relationship,source: :follower
+
+  has_many :relationships,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :followings,through: :relationships,source: :followed
+
+  def follow(user_id)
+    relationships.create(followed_id:user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id:user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
   validates :name,uniqueness:true,length:{ in:2..20 }
   validates :introduction,length:{ maximum:50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   root to:'homes#top'
   get 'home/about'=>'homes#about'
   resources :books,only:[:index,:show,:edit,:create,:destroy,:update]
-  resources :users,only:[:index,:show,:edit,:create,:update]
+  resources :users,only:[:index,:show,:edit,:create,:update] do
+    resource :relationships,only:[:create,:destroy]
+    get 'followings'=>'relationship#followings',as:'followings'
+    get 'followers'=>'relationship#followers',as:'followers'
+  end
 end
